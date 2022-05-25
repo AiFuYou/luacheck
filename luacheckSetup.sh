5bc652e45eb9e0064aa64fd1cf20f53daa2b9970
suyongfu <821314629@qq.com>
2022-05-25 12:48:28 +0800

    update dstPath

--- luacheckSetup.sh
+++ luacheckSetup.sh
@@ -1,7 +1,7 @@
 echo off
 echo copy pre-commit to .git/hooks/
 # 根据项目情况修改以下路径
-dstPath=./.git/hooks/pre-commit
+dstPath=../.git/hooks/pre-commit
 cp pre-commit $dstPath
 chmod 777 $dstPath
 echo install luarocks and luacheck
