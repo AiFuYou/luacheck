09804a1ef5a1122fd40515f107a7a63d3a2a0e9d
suyongfu <821314629@qq.com>
2022-05-26 22:59:49 +0800

    luacheckSetup log better

--- luacheckSetup.sh
+++ luacheckSetup.sh
@@ -1,11 +1,33 @@
-echo off
-echo copy pre-commit to .git/hooks/
+echoRed(){
+	echo "\033[31m$@\033[0m"
+}
+
+echoGreen(){
+	echo "\033[32m$@\033[0m"
+}
+
+echoGreen copy pre-commit to .git/hooks/
 # 根据项目情况修改以下路径
 dstPath=../.git/hooks/pre-commit
 cp pre-commit $dstPath
 chmod 777 $dstPath
-echo install luarocks and luacheck
-sudo chown -R $(whoami) /usr/local/*
-brew install luarocks
-luarocks install luacheck
-echo done
+echo checking luarocks install
+which luarocks
+if [[ $? != 0 ]]; then
+	echoGreen install luarocks
+	sudo chown -R $(whoami) /usr/local/*
+	brew install luarocks
+else
+	echoGreen luarocks already exists
+fi
+
+echo checking luacheck install
+which luacheck
+if [[ $? != 0 ]]; then
+	echoGreen install luacheck
+	luarocks install luacheck
+else
+	echoGreen luacheck already exists
+fi
+
+echoGreen done
